f33d7e91cbc16d66a659229457b52890d79ce288
Zakir Hossain <zakirh817@gmail.com>
2022-11-26 23:38:53 +0600

    pages created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Toaster } from 'react-hot-toast';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './routes/Routes';
 
 function App() {
   return (
-    <div className="">
+    <RouterProvider router={routes} className="">
       <h1>Development Start</h1>
       <Toaster />
-    </div>
+    </RouterProvider>
   );
 }
 
